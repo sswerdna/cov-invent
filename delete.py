924a9d6cb9d2818f63919ce0c922df1eb8da7424
Stephen Andrews <stephen@mint>
2012-06-06 17:45:36 -0500

    Added basic (CRUD) functionalities. These were already implemented when the project was ported over to git source tracking. The service performs very basic inventory operations, including tracking where lots are, and associating part numbers with lot numbers. It also understands the lot number as primary key, and responds to lot number duplication by updating, rather than adding a duplicate lot. Also, the filename pointed to in the files is a local file, so if you want to use it, you'll have to alter it.

---
+++ delete.py
@@ -0,0 +1,24 @@
+from Tkinter import *
+import sqlite3 as sql, main
+
+filename = "/home/stephen/inventory/~invdb.db"
+def go(root):	
+	root.destroy()
+	master = Tk()
+	splash = Label(master,text = "Enter the lot number of the item you would like to delete.")
+	splash.grid(row = 0, column = 0, columnspan = 2, padx = 3, pady = 3)
+	del_label = Label(master,text = "Lot Number: ")
+	del_label.grid(row = 1, column = 0, padx = 3, pady = 3)
+	delbox = Entry(master)
+	delbox.grid(row = 1, column = 1, padx = 3, pady = 3)
+	def remove_item():
+		conn = sql.connect(filename)
+		curs = conn.cursor()
+		curs.execute("DELETE FROM items WHERE lot_number=%s" % delbox.get())
+		reset(master)
+	del_button = Button(master, text = "Delete", command = remove_item)
+	del_button.grid(row = 2, column = 1, padx = 3, pady = 3)
+	master.mainloop()
+def reset(master):
+	master.destroy()
+	main.go()
